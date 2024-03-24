d20bc922b5cdb44390d1d4ccc2b20b2de2fdca51
EatMyGoose <dellich@gmail.com>
2024-03-24 16:45:47 +0800

    Long filenames are now truncated in the Transcription history sidebar

--- WebUI/src/Components/JobStatusPanel.tsx
+++ WebUI/src/Components/JobStatusPanel.tsx
@@ -1,6 +1,7 @@
 import { Endpoints } from "../Endpoints/Endpoints";
 import { IsJobReady } from "../types"
 import { cx } from "../util"
+import util from "../util.module.css"
 import styles from "./JobStatusPanel.module.css"
 
 export interface IJobHistoryPanel
@@ -17,16 +18,16 @@ export function JobHistoryPanel(props: IJobHistoryPanel)
     return (
         <li className="collection-item">
             <div className={cx(styles.panel)}>
-                <div className={styles.info}>
+                <div className={cx(styles.info, util.truncate_content)}>
                     <p>ID: {props.id}</p>
                     <p>Filename: {props.filename}</p>
                     <p>Status: {props.status}</p>
                 </div>
-                <div className="valign-wrapper">
+                <div className={cx("valign-wrapper", styles.download)}>
                     <a 
                         href={downloadURL}
                         download
-                        className={cx("btn-flat", readyForDownload? "" : "disabled")}
+                        className={cx("btn-flat", util.px_0, util.px0, readyForDownload? "" : "disabled")}
                     >
                         <i className="small material-icons">file_download</i>
                     </a>
